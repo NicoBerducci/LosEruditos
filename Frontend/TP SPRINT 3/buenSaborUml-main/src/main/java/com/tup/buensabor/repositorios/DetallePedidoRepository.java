3010c59e14a72ec9db271b2f2d08e7feeded2f52
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-10-04 22:28:16 -0300

    Add files via upload

---
+++ Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/DetallePedidoRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.DetallePedido;
+
+public interface DetallePedidoRepository extends BaseRepository<DetallePedido,Long> {
+}
---
+++ Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/DomicilioRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.Domicilio;
+
+public interface DomicilioRepository extends BaseRepository <Domicilio,Long>{
+}
