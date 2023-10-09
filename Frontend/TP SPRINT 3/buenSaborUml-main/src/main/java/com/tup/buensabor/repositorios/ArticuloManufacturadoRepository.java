52835c054547157981828f873fb42581ffe1bbf8
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-10-09 14:15:39 -0300

    Delete Frontend/TP SPRINT 3/buenSaborUml-main directory

--- Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/ArticuloManufacturadoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tup.buensabor.repositorios;
-
-import com.tup.buensabor.entities.ArticuloManufacturado;
-
-public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado,Long>{
-}
--- Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tup.buensabor.repositorios;
-
-import com.tup.buensabor.entities.Usuario;
-
-public interface UsuarioRepository extends BaseRepository<Usuario,Long>{
-}
