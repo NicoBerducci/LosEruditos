e8d6edd2738536ed2219577b7abfadb280f0b59f
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-10-09 14:20:57 -0300

    carpeta actualizada- la anterior estaba desactualizada
    Habíamos realizado cambios en la carpeta antes del fin de semana pero subimos una copia desactualizada.

---
+++ Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/PedidoRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.Pedido;
+
+public interface PedidoRepository extends BaseRepository<Pedido,Long> {
+}
---
+++ Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/RubroArticuloManufacturadoRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.RubroArticuloManufacturado;
+
+public interface RubroArticuloManufacturadoRepository extends BaseRepository<RubroArticuloManufacturado,Long> {
+}
