8d7e4c4785438c2f12e961f0b21cf89eb9ed37a9
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-10-09 14:18:19 -0300

    Carpeta buena- la anterior estaba desactualizada

---
+++ Frontend/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/ArticuloManufacturadoRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.ArticuloManufacturado;
+
+public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado,Long>{
+}
---
+++ Frontend/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/UnidadMedidaRepository.java
@@ -0,0 +1,6 @@
+package com.tup.buensabor.repositorios;
+
+import com.tup.buensabor.entities.UnidadMedida;
+
+public interface UnidadMedidaRepository extends BaseRepository<UnidadMedida,Long>{
+}
