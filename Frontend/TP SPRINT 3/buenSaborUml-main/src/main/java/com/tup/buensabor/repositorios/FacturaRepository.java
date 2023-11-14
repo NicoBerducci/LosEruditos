c590ea816cb4a974476bcee85ca733c65d72941d
NicoBerducci <142543849+NicoBerducci@users.noreply.github.com>
2023-11-14 20:57:46 -0300

    Delete Frontend directory

--- Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/FacturaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tup.buensabor.repositorios;
-
-import com.tup.buensabor.entities.Factura;
-
-public interface FacturaRepository extends BaseRepository<Factura,Long> {
-}
--- Frontend/TP SPRINT 3/buenSaborUml-main/src/main/java/com/tup/buensabor/repositorios/RubroArticuloRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tup.buensabor.repositorios;
-
-import com.tup.buensabor.entities.RubroArticulo;
-
-public interface RubroArticuloRepository extends BaseRepository <RubroArticulo,Long>{
-}
